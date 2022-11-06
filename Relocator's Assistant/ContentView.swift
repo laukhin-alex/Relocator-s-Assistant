da120902c075641afb5bd6bc8f738c002465ac57
Alex <alex.laukhin@yandex.ru>
2022-11-06 14:20:31 +0400

    TSA was added

--- Relocator's Assistant/ContentView.swift
+++ Relocator's Assistant/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text(CountriesFlags.armeniaFlag)
+                .font(.system(size: 200))
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
