5ccc546d20c35065a28d3f38ca9125de514e7517
laukhin-alex <alex.laukhin@yandex.ru>
2022-11-06 12:48:06 +0400

    Initial Commit

---
+++ Relocator's Assistant/Relocator_s_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Relocator_s_AssistantApp.swift
+//  Relocator's Assistant
+//
+//  Created by Александр on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Relocator_s_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
